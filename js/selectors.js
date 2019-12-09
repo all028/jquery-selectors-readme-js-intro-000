cb71b9a9eeb67c2ed317b0ec7d14009f0e19074b
al <albliu@email.com>
2019-12-09 19:09:27 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,20 @@
 function paragraphSelector(){
-  
+
 }//end paragraphSelector
 
 function lastImageSelector(){
-  
+
 }//end lastImageSelector
 
 function ninjaBabySelector(){
-  
+
 }//end ninjaBabySelector
 
 function divSelector(){
 
-  
+
 }//end divSelector
 
 function firstListItem(){
-  
+
 }//end firstListItem
